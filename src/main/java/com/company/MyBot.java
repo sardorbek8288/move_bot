d42977da077b496878a93bb267edb713d14ba164
Sardorbek <sardorbekf51@gmail.com>
2025-02-06 15:37:33 +0500

    ends

--- src/main/java/com/company/MyBot.java
+++ src/main/java/com/company/MyBot.java
@@ -24,72 +24,11 @@ public class MyBot extends TelegramLongPollingBot {
         if (update.getMessage().getChat().getId() == -1002499280744L){
             saveMovie(update.getMessage());
         }else {
-            getVideoById(update.getMessage().getText() , update.getMessage().getChat());
+
         }
     }
 
-    @SneakyThrows
-    private void getVideoById(String text, Chat chat){
 
-        String sql = "select * from movie where id = ?";
-        try(Connection connection = db.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);){
-
-            Integer i = Integer.valueOf(text);
-            preparedStatement.setInt(1, i);
-
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            if (!resultSet.next()) {
-                SendMessage sendMessage = new SendMessage();
-                sendMessage.setText("Xozircha bu Id da xali kino yo'q!");
-                sendMessage.setChatId(chat.getId());
-                execute(sendMessage);
-            } else {
-                do {
-                    String movieId = resultSet.getString("movie_id");
-                    String name = resultSet.getString("name");
-                    sendVideo(chat.getId(), movieId, name);
-                } while (resultSet.next());
-            }
-
-        }catch (Exception e){
-            String sql1 = "select * from movie where name = ?";
-            try(Connection connection = db.getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(sql1);){
-
-                preparedStatement.setString(1, text);
-
-
-                ResultSet resultSet = preparedStatement.executeQuery();
-
-                if (!resultSet.next()) {
-                    SendMessage sendMessage = new SendMessage();
-                    sendMessage.setText("Xozircha bu nomli kino yo'q!");
-                    sendMessage.setChatId(chat.getId());
-                    execute(sendMessage);
-                } else {
-                    do {
-                        String movieId = resultSet.getString("movie_id");
-                        String name = resultSet.getString("name");
-                        sendVideo(chat.getId(), movieId, name);
-                    } while (resultSet.next());
-                }
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        }
-    }
-
-    @SneakyThrows
-    private void sendVideo(Long id, String movieId, String name) {
-        SendVideo sendVideo = new SendVideo();
-        sendVideo.setChatId(id);
-        sendVideo.setVideo(new InputFile(movieId));
-        sendVideo.setCaption(name);
-        execute(sendVideo);
-    }
 
 
     private void saveMovie(Message message) {
